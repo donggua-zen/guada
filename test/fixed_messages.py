0e29be39d798e500dec8b62c4a6ea2c5c54cedf8
冬瓜 <2207134109@qq.com>
2025-09-29 00:55:26 +0800

    更新测试文件

---
+++ test/fixed_messages.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import time
+
+
+# 获取当前脚本的绝对路径
+current_script_path = os.path.abspath(__file__)
+
+# 获取当前脚本所在的目录
+current_directory = os.path.dirname(current_script_path)
+sys.path.append(os.path.dirname(current_directory))
+os.chdir(os.path.dirname(current_directory))
+
+from message_service import get_message_service
+from session_service import get_session_service
+
+sessions = get_session_service().get_all_sessions()
+
+for session in sessions:
+    messages = get_message_service().get_messages(session["id"])
+    for i, message in enumerate(messages):
+        print(f"{i + 1}. {message['content']}")
+        if i > 0:
+            get_message_service().update_message(
+                message_id=message["id"], data={"parent_id": messages[i - 1]["id"]}
+            )
+        else :
+            get_message_service().update_message(
+                message_id=message["id"], data={"parent_id": None}
+            )
