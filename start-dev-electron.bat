1f9e319f1938baf7df211cdf05439efba136e8ca
冬瓜 <2207134109@qq.com>
2026-05-06 09:50:36 +0800

    修改快捷脚本

--- start-dev-electron.bat
+++ start-dev-electron.bat
@@ -5,9 +5,7 @@ echo   Electron 后端调试模式
 echo ========================================
 echo.
 echo 正在启动...
-echo   - 后端：热重载模式 (ts-node-dev)
-echo   - 前端：使用编译后的文件
-echo   - DevTools：自动打开
+
 echo.
 echo 提示：按 Ctrl+C 可停止所有进程
 echo ========================================
@@ -16,6 +14,6 @@ echo.
 cd /d "%~dp0"
 
 :: 启动调试模式
-npm run dev:electron:prod-frontend
+npm run dev:electron
 
 pause
